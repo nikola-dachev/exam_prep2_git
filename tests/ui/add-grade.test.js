1c71566f9e47b26c847fc1e556ca235b59c7a325
nikola-dachev <126467609+nikola-dachev@users.noreply.github.com>
2024-12-30 15:51:05 +0200

    fixing tests

--- tests/ui/add-grade.test.js
+++ tests/ui/add-grade.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add grades page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-grade');
+    await page.goto('http://localhost:8080/Add-Grade');
     const form = await page.$('textfield');
     expect(form).toBeTruthy();
   });
--- tests/ui/grades.test.js
+++ tests/ui/grades.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check grades page', async ({ page }) => {
-    await page.goto('http://localhost:8080/my-grades');
+    await page.goto('http://localhost:8080/My-Grades');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
